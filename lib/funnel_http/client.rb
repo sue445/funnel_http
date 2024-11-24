b0054d7a25c23303a0587b1dfc1306d35dd915cb
sue445 <sue445@sue445.net>
2024-11-24 11:48:02 +0900

    Add definition

--- lib/funnel_http/client.rb
+++ lib/funnel_http/client.rb
@@ -2,5 +2,19 @@
 
 module FunnelHttp
   class Client
+    # perform HTTP requests in parallel
+    #
+    # @param requests [Array<Hash{String => String}>] `Array` of following `Hash`
+    # @option requests :method [String, Symbol] Request method (e.g. `:get`, `"POST"`)
+    # @option requests :url [String] Request url
+    # @option requests :header [Hash{String, => String, Array<String>}] Request header
+    #
+    # @return [Array<Hash<Symbol, Object>>] `Array` of following `Hash`
+    # @return [Integer] `:status_code`
+    # @return [String] `:body` Response body
+    # @return [Hash{String, => String, Array<String>}] `:header` Response header
+    def perform(requests)
+      []
+    end
   end
 end
