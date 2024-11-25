c91de89d68628f3177a8f0a5420af869374fc71f
sue445 <sue445@sue445.net>
2024-11-25 23:43:01 +0900

    nits: Surrounded by context

--- spec/funnel_http/client_spec.rb
+++ spec/funnel_http/client_spec.rb
@@ -8,39 +8,41 @@ RSpec.describe FunnelHttp::Client do
 
     subject(:responses) { client.perform(requests) }
 
-    let(:requests) do
-      [
-        {
-          method: "GET",
-          url: "#{test_server}/get",
-          header: {"X-Test-Header" => ["a", "b"]}
-        },
-        {
-          method: "GET",
-          url: "#{test_server}/get",
-          header: {"X-Test-Header" => ["c", "d"]}
-        },
-      ]
-    end
+    context "simple case" do
+      let(:requests) do
+        [
+          {
+            method: "GET",
+            url: "#{test_server}/get",
+            header: {"X-Test-Header" => ["a", "b"]}
+          },
+          {
+            method: "GET",
+            url: "#{test_server}/get",
+            header: {"X-Test-Header" => ["c", "d"]}
+          },
+        ]
+      end
 
-    its(:count) { should eq 2 }
+      its(:count) { should eq 2 }
 
-    describe "[0]" do
-      subject { responses[0] }
+      describe "[0]" do
+        subject { responses[0] }
 
-      its([:status_code]) { should eq 200 }
-      its([:body]) { should eq "/get" }
-      its([:header]) { should include("Content-Type" => ["text/plain;charset=utf-8"]) }
-      its([:header]) { should include("X-Request-Headers" => [include('"HTTP_X_TEST_HEADER"=>"a, b"')]) }
-    end
+        its([:status_code]) { should eq 200 }
+        its([:body]) { should eq "/get" }
+        its([:header]) { should include("Content-Type" => ["text/plain;charset=utf-8"]) }
+        its([:header]) { should include("X-Request-Headers" => [include('"HTTP_X_TEST_HEADER"=>"a, b"')]) }
+      end
 
-    describe "[1]" do
-      subject { responses[1] }
+      describe "[1]" do
+        subject { responses[1] }
 
-      its([:status_code]) { should eq 200 }
-      its([:body]) { should eq "/get" }
-      its([:header]) { should include("Content-Type" => ["text/plain;charset=utf-8"]) }
-      its([:header]) { should include("X-Request-Headers" => [include('"HTTP_X_TEST_HEADER"=>"c, d"')]) }
+        its([:status_code]) { should eq 200 }
+        its([:body]) { should eq "/get" }
+        its([:header]) { should include("Content-Type" => ["text/plain;charset=utf-8"]) }
+        its([:header]) { should include("X-Request-Headers" => [include('"HTTP_X_TEST_HEADER"=>"c, d"')]) }
+      end
     end
   end
 end
