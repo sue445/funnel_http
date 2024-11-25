8090e41b8bb0656a719f4af2a3c1f34de344ca1d
sue445 <sue445@sue445.net>
2024-11-25 23:55:17 +0900

    Tweak yard

--- lib/funnel_http/client.rb
+++ lib/funnel_http/client.rb
@@ -4,10 +4,17 @@ module FunnelHttp
   class Client
     # perform HTTP requests in parallel
     #
-    # @param requests [Array<Hash{String => String}>] `Array` of following `Hash`
-    # @option requests :method [String, Symbol] Request method (e.g. `:get`, `"POST"`)
-    # @option requests :url [String] Request url
-    # @option requests :header [Hash{String, => String, Array<String>}] Request header
+    # @overload perform(requests)
+    #   @param requests [Array<Hash{String => String}>] `Array` of following `Hash`
+    #   @option requests :method [String, Symbol] Request method (e.g. `:get`, `"POST"`)
+    #   @option requests :url [String] Request url
+    #   @option requests :header [Hash{String, => String, Array<String>}] Request header
+    #
+    # @overload perform(request)
+    #   @param request [Hash{String => String}]
+    #   @option request :method [String, Symbol] Request method (e.g. `:get`, `"POST"`)
+    #   @option request :url [String] Request url
+    #   @option request :header [Hash{String, => String, Array<String>}] Request header
     #
     # @return [Array<Hash<Symbol, Object>>] `Array` of following `Hash`
     # @return [Integer] `:status_code`
