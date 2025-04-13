c1b7a4fb87e220487e7ccbcc180fa73dee35b6f6
sue445 <sue445@sue445.net>
2025-04-13 22:48:34 +0900

    Move Error to errors

--- lib/funnel_http.rb
+++ lib/funnel_http.rb
@@ -3,11 +3,10 @@
 require_relative "funnel_http/version"
 require_relative "funnel_http/client"
 require_relative "funnel_http/ext"
+require_relative "funnel_http/errors"
 require_relative "funnel_http/funnel_http"
 
 module FunnelHttp
-  class Error < StandardError; end
-
   USER_AGENT = "funnel_http/#{FunnelHttp::VERSION} (+https://github.com/sue445/funnel_http)"
 
   # Your code goes here...
