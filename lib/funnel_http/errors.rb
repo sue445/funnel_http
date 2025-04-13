e56944f532fa158087aaa6ca01e105453acf9d6d
sue445 <sue445@sue445.net>
2025-04-13 22:54:54 +0900

    Impl `HttpAggregateError.generate_error_message`

--- lib/funnel_http/errors.rb
+++ lib/funnel_http/errors.rb
@@ -2,4 +2,16 @@
 
 module FunnelHttp
   class Error < StandardError; end
+
+  # Aggregates multiple http errors
+  class HttpAggregateError < Error
+    # Generate error message for [StandardError#initialize]
+    #
+    # @param error_responses [Array<Hash<Symbol => Object>>]
+    #
+    # @return [String]
+    def self.generate_error_message(error_responses)
+      error_responses.map { |res| "#{res[:url]} (#{res[:status_code]} error)" }.join(", ")
+    end
+  end
 end
